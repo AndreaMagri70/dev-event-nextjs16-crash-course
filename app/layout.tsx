a6a94aa5df9ffba556de1d51432b790e16039168
AndreaMagri70 <assomag@libero.it>
2025-12-31 10:45:31 +0100

    Update layout.tsx and global.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
-import "./globals.css";
+
 import LightRays from "@/components/LightRays";
 import Navbar from "@/components/Navbar";
 
