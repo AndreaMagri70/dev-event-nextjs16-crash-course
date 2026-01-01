a9d3252e86e360178eb76259dc8e6dfe180acb2c
AndreaMagri70 <assomag@libero.it>
2026-01-01 18:50:19 +0100

    Event details Page - prima parte

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,6 +1,19 @@
-const EventDetailsPage = () => {
+import { notFound } from "next/navigation";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const EventDetailsPage = async ({ params}: { params: Promise<{ slug: string }>}) => {
+  const { slug } = await params;
+  const request = await fetch(`${BASE_URL}/api/events/${slug}`);
+  const { event } = await request.json();
+
+  if(!event) return notFound();
+
+
   return (
-    <div>Event Details Page</div>
+    <section id="event">
+      <h1>Event Details: <br/> {event.title}</h1>
+    </section>
   )
 }
 
