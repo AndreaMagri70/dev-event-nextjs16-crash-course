e3951fbb0273ac566c5185c106c27992514618a0
AndreaMagri70 <assomag@libero.it>
2026-01-01 18:07:42 +0100

    Add of the API manage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import EventCard from '@/components/EventCard';
 import ExploreBtn from '@/components/ExploreBtn';
-import events from '@/lib/costants';
+import { IEvent } from '@/database';
 
 import React from 'react';
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className='text-center'>The Hub for Every Dev <br/> Event You Can't Miss</h1>
@@ -16,7 +21,7 @@ const Page = () => {
         <h3>Featured Events</h3>
 
         <ul className='events list-none my-6' id='events'>
-          {events.map((event) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title}>
               <EventCard {...event } />
             </li>
